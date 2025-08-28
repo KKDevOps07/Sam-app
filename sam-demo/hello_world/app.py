427d6c7ac13bf5d68fec24140b49184158ba6224
root <root@ip-172-31-25-102.ec2.internal>
2025-08-28 07:42:34 +0000

    Changed return message

--- sam-demo/hello_world/app.py
+++ sam-demo/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello this is kiran i am learning Serveless CI/CD",
+            "message": "Serveless CI/CD, using Canary deployments",
             # "location": ip.text.replace("\n", "")
         }),
     }
