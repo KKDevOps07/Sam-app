bfd6de08f6fc36f8435c032bd2dfcac113f87520
root <root@ip-172-31-25-102.ec2.internal>
2025-08-28 07:04:35 +0000

    Updated app.py to test the CI/CD

--- sam-demo/hello_world/app.py
+++ sam-demo/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello KK",
+            "message": "hello this is kiran i am learning Serveless CI/CD",
             # "location": ip.text.replace("\n", "")
         }),
     }
